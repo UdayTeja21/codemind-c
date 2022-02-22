dc5bce2098181db17a2d7c1866582ee0d7b0dcc0
UdayTeja21 <97437700+UdayTeja21@users.noreply.github.com>
2022-02-22 15:37:01 +0530

    #include <stdio.h> int main() {     int q, a, b, j, n = 0;     scanf("%d%d", &a, &b);     for (j = a; j < b; j++,a++)     {n=0;         while (j != 0)         {             q = j % 10;             j /= 10;             n = q + n * 10;         }         if (a == n)             {printf("%d ", a);}         j=a;     }     return 0; }

---
+++ Program_to_print_all_palindromes_in_a_given_range_.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int q, a, b, j, n = 0;
+    scanf("%d%d", &a, &b);
+    for (j = a; j < b; j++,a++)
+    {n=0;
+        while (j != 0)
+        {
+            q = j % 10;
+            j /= 10;
+            n = q + n * 10;
+        }
+        if (a == n)
+            {printf("%d ", a);}
+        j=a;
+    }
+    return 0;
+}
