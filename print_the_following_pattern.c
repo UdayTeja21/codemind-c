e7e4aeaa607e63c709a39266c91e6ff2e737137f
UdayTeja21 <97437700+UdayTeja21@users.noreply.github.com>
2022-02-26 21:56:38 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(j=n;j>0;j--)     {         for(i=1;i<=j;i++)     {         printf("%d",i);     }     printf(" ");     }     return 0; }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(j=n;j>0;j--)
+    {
+        for(i=1;i<=j;i++)
+    {
+        printf("%d",i);
+    }
+    printf("
+");
+    }
+    return 0;
+}
